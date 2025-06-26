6a0f7c48dea661670e0938e57781e314ac3c79a2
Sebastian Rombach <Sebastian.Rombach@t-online.de>
2025-06-26 10:03:31 +0200

    split data into train, test, validation after download

--- data/load_data.py
+++ data/load_data.py
@@ -1,9 +1,12 @@
 import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # === Config ===
 DATASET = "alexteboul/diabetes-health-indicators-dataset"
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-TARGET_DIR = os.path.join(SCRIPT_DIR, "raw")
+RAWDATA_DIR = os.path.join(SCRIPT_DIR, "raw")
+SPLIT_DATA_DIR = os.path.join(SCRIPT_DIR, "split")
 KAGGLE_JSON = os.path.expanduser("~/.kaggle/kaggle.json")
 
 # === Check if kaggle.json exists ===
@@ -26,6 +29,19 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 # === Download dataset ===
 api = KaggleApi()
 api.authenticate()
-api.dataset_download_files(dataset=DATASET, path=TARGET_DIR, unzip=True)
+api.dataset_download_files(dataset=DATASET, path=RAWDATA_DIR, unzip=True)
 
-print(f"✅ Downloaded: '{DATASET}' → '{TARGET_DIR}'")
+print(f"✅ Downloaded: '{DATASET}' into '{RAWDATA_DIR}'")
+
+
+os.makedirs(SPLIT_DATA_DIR, exist_ok=True)
+
+df_raw = pd.read_csv(os.path.join(RAWDATA_DIR, "diabetes_012_health_indicators_BRFSS2015.csv"))
+
+X_train, X_temp = train_test_split(df_raw, random_state=42, test_size=0.15)
+X_test, X_val = train_test_split(X_train, random_state=42, test_size=0.66)
+
+
+X_train.to_csv(os.path.join(SPLIT_DATA_DIR, "train_raw_split.csv"), index=False)
+X_test.to_csv(os.path.join(SPLIT_DATA_DIR, "test_raw_split.csv"), index=False)
+X_val.to_csv(os.path.join(SPLIT_DATA_DIR, "validation_raw_split.csv"), index=False)
